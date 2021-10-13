d041c53c231e886aaed24d26148e227d50d33c08
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-10-13 15:29:21 -0300

    counter

--- FINAL PROJECT/javascript/slider.js
+++ FINAL PROJECT/javascript/slider.js
@@ -4,8 +4,16 @@
  var sliderContainer = document.querySelector('.jl-slider-container');
  var sliderList = document.querySelector('.jl-slider-list');
  var sliderItems = document.querySelectorAll('.jl-slider-item');
+ const sliderTotalItems = sliderItems.length;
  var sliderListWidth = null;
+ var prevItem = document.querySelector('.jl-item-prev');
+ var nextItem = document.querySelector('.jl-item-next');
+ var sliderPos = 0;
+ var currentSlide = document.querySelector('.jl-current-slide');
+ var totalSlide = document.querySelector('.jl-total-slide');
+ var currentCounter = 1;
 
+ 
  //capturando largura individuais
  var containerWidth = sliderContainer.parentElement.offsetWidth;
  
@@ -25,9 +33,6 @@
 
  // Fazendo animação do slider onClick
 
- var prevItem = document.querySelector('.jl-item-prev');
- var nextItem = document.querySelector('.jl-item-next');
- var sliderPos = 0;
 
  //HANDLERS
  var nextSliderAnime = function(){
@@ -40,7 +45,6 @@
          targets: sliderList,
          translateX: sliderPos
      });
-     console.log(sliderPos)
  }
 
  var prevSliderAnime = function(){
@@ -52,14 +56,47 @@
           targets: sliderList,
           translateX: sliderPos
       });
-      console.log(sliderPos)
 
  }
+
+ // Counter Formater
+
+ var counterFormater = function(number){
+   if(number < 10){
+       return '0'+number;
+   }else{
+       return number;
+   }
+ }
+
+ //Counter add
+ var counterAdd = function(){
+     if((currentCounter >= 0) && (currentCounter < sliderTotalItems)){ 
+        currentCounter++;
+        currentSlide.innerHTML= counterFormater(currentCounter);
+    }
+  }
+
+  //Counter remove
+ var counterRemove = function(){
+    if((currentCounter > 1) && (currentCounter <= sliderTotalItems)){ 
+       currentCounter--;
+       currentSlide.innerHTML= counterFormater(currentCounter);
+   }
+ }
+
+
+ // ACTIONS
+ totalSlide.innerHTML=counterFormater(sliderTotalItems);
+
+
  nextItem.addEventListener('click', function(){
      nextSliderAnime();
+     counterAdd();
  });
 
  prevItem.addEventListener('click', function(){
      prevSliderAnime();
+     counterRemove();
    
 });
